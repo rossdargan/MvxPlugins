795f1acf54cb693aa04071a461f6852776864731
rossdargan <git@the-dargans.co.uk>
2013-12-28 16:03:19 +0000

    Release version 1.0.1

---
+++ NuSpec/TouchNFCPluginBootstrap.cs.pp
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace $rootnamespace$.Bootstrap
+{
+public class NFCPluginBootstrap
+: MvxLoaderPluginBootstrapAction<DevelopingTrends.MvxPlugins.NFC.PluginLoader, DevelopingTrends.MvxPlugins.NFC.Touch.Plugin>
+    {
+    }
+}
---
+++ NuSpec/pack.bat
@@ -0,0 +1,2 @@
+del *.nupkg
+nuget pack DevelopingTrends.MvxPlugins.Nfc.nuspec
